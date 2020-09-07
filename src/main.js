2db4641c8fc2d02b0bf49ccf023688422339204c
zeyura <zeyura75@gmail.com>
2020-09-07 16:33:06 +0300

    Почистил сборку в ноль, можно начинать)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+
+import router from './router/router'
+import store from './store/store'
 
 Vue.config.productionTip = false
 
