b5eaa533be4c160c0b7efd45c0c23b42b2bba5ba
zeyura <zeyura75@gmail.com>
2020-09-07 19:15:53 +0300

    создал страницы в папке views и настроил роуты

--- src/router/router.js
+++ src/router/router.js
@@ -8,6 +8,7 @@ const routes = [
     {
         path: '/',
         name: 'Home',
+        meta: {layout: 'main'},
         component: Home
     },
     {
@@ -17,17 +18,53 @@ const routes = [
         component: () => import('../views/Login.vue')
     },
     {
+        path: '/register',
+        name: 'Register',
+        meta: {layout: 'empty'},
+        component: () => import('../views/Register.vue')
+    },
+    {
         path: '/categories',
         name: 'Categories',
         meta: {layout: 'main'},
         component: () => import('../views/Categories.vue')
+    },
+    {
+        path: '/detail-record',
+        name: 'DetailRecord',
+        meta: {layout: 'main'},
+        component: () => import('../views/DetailRecord.vue')
+    },
+    {
+        path: '/history',
+        name: 'History',
+        meta: {layout: 'main'},
+        component: () => import('../views/History.vue')
+    },
+    {
+        path: '/planning',
+        name: 'Planning',
+        meta: {layout: 'main'},
+        component: () => import('../views/Planning.vue')
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        meta: {layout: 'main'},
+        component: () => import('../views/Profile.vue')
+    },
+    {
+        path: '/record',
+        name: 'Record',
+        meta: {layout: 'main'},
+        component: () => import('../views/Record.vue')
     }
-]
+];
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes
-})
+});
 
 export default router
