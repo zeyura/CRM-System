7626f4eb82a4443b154edec3499f7220f4dc2cd0
zeyura <zeyura75@gmail.com>
2020-09-07 21:45:58 +0300

    создал компоненты сайдбар и навбар; сделал сайдбар скрываемым

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <ul class="sidenav app-sidenav " :class="{'open': isOpen}">
+        <router-link
+            v-for="link in links"
+            :key="link.url"
+            tag="li"
+            active-class="active"
+            :to="link.url"
+            :exact="link.exact"
+        >
+            <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+        </router-link>
+    </ul>
+
+</template>
+
+<script>
+    export default {
+        name: "sidebar",
+        props: ['isOpen'],
+        data: () => ({
+            links: [
+                {title: 'Счет', url: '/', exact: true },
+                {title: 'История', url: '/history' },
+                {title: 'Планирование', url: '/planning' },
+                {title: 'Новая запись', url: '/record' },
+                {title: 'Категории', url: '/categories' },
+            ]
+        }),
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
