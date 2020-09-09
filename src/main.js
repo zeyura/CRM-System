da7a4ea4a614e1482700cd1bb57589513c2b4e50
zeyura <zeyura75@gmail.com>
2020-09-09 12:29:03 +0300

    настроил валидацию  форм на стр  login & register с помощью vuelidate

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router/router'
 import store from './store/store'
 
+Vue.use(Vuelidate);
 import dateFilter from './filters/date.filter'
 
 import 'materialize-css/dist/js/materialize.min'
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,37 +1,53 @@
 <template>
 
-    <form class="card auth-card">
+    <form class="card auth-card" @submit.prevent="submitHandler">
         <div class="card-content">
             <span class="card-title">Домашняя бухгалтерия</span>
             <div class="input-field">
                 <input
                         id="email"
                         type="text"
+                        v-model.trim="email"
+                        :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email) }"
                 >
                 <label for="email">Email</label>
-                <small class="helper-text invalid">Email</small>
+                <small class="helper-text invalid"
+                       v-if="$v.email.$dirty && !$v.email.required"
+                >Поле не должно быть пустым</small>
+                <small class="helper-text invalid"
+                       v-else-if="$v.email.$dirty && !$v.email.email"
+                >Введите корректный Email</small>
             </div>
             <div class="input-field">
                 <input
                         id="password"
                         type="password"
-                        class="validate"
+                        v-model.trim="password"
+                        :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength) }"
                 >
                 <label for="password">Пароль</label>
-                <small class="helper-text invalid">Password</small>
+                <small class="helper-text invalid"
+                       v-if="$v.password.$dirty && !$v.password.required"
+                >Поле не должно быть пустым</small>
+                <small class="helper-text invalid"
+                       v-if="$v.password.$dirty && !$v.password.minLength"
+                >Пароль должен содержать мин {{$v.password.$params.minLength.min}} символов. Сейчас {{password.length}}</small>
             </div>
             <div class="input-field">
                 <input
                         id="name"
                         type="text"
-                        class="validate"
+                        v-model.trim="name"
+                        :class="{invalid: ($v.name.$dirty && !$v.name.required) }"
                 >
                 <label for="name">Имя</label>
-                <small class="helper-text invalid">Name</small>
+                <small class="helper-text invalid"
+                       v-if="$v.name.$dirty && !$v.name.required"
+                >Введите ваше имя</small>
             </div>
             <p>
                 <label>
-                    <input type="checkbox"/>
+                    <input type="checkbox" v-model="agree" />
                     <span>С правилами согласен</span>
                 </label>
             </p>
@@ -49,7 +65,7 @@
 
             <p class="center">
                 Уже есть аккаунт?
-                <a href="/">Войти!</a>
+                <router-link to="/login">Войти!</router-link>
             </p>
         </div>
     </form>
@@ -57,8 +73,43 @@
 </template>
 
 <script>
+    import {email, required, minLength} from 'vuelidate/lib/validators'
     export default {
-        name: "register"
+        name: "register",
+        data: () => ({
+            email: '',
+            password: '',
+            name:  '',
+            agree: false
+        }),
+        validations: { // после установки vuelidate /  npm install vuelidate --save
+            email: {email, required},
+            password: {required, minLength: minLength(6)}, // min 6 symbols
+            name: {required},
+            // для agree пишем свой валидатор checked - он должен возвращать труе   checked: v => v
+            agree: {checked: v => v}  //  return true ^)
+        },
+        methods: {
+            submitHandler() {
+
+                if(this.$v.$invalid) { // if Form is in Invalid
+                    this.$v.$touch();
+                    return
+                }
+
+                const formData = {
+                    email: this.email,
+                    password: this.password,
+                    name: this.name,
+                   // agree: this.agree
+                };
+
+               /// console.info(formData)
+
+                this.$router.push('/')
+            }
+        }
+
     }
 </script>
 
