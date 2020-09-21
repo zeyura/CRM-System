18282047b9e832e42746d070004da10d229a01bf
zeyura <zeyura75@gmail.com>
2020-09-21 22:28:06 +0300

    сделал диаграмму чарта - на стр история записей с помощью vue-chartjs

--- src/views/History.vue
+++ src/views/History.vue
@@ -6,7 +6,7 @@
         </div>
 
         <div class="history-chart">
-            <canvas></canvas>
+            <canvas ref="canvas"></canvas>
         </div>
 
         <section>
@@ -42,36 +42,79 @@
 <script>
     import PaginationMixin from '@/mixins/pagination.mixin'
     import HistoryTable from '@/components/HistoryTable'
+    import {Pie} from 'vue-chartjs'
 
     export default {
         name: "history",
         components: { HistoryTable },
         mixins: [PaginationMixin],
+        extends: Pie,
         data: () => ({
             loading: true,
             records: []
         }),
         async mounted() {
-
             this.records = await this.$store.dispatch('fetchRecords');
             //const records   = await this.$store.dispatch('fetchRecords');
             const categories = await this.$store.dispatch('fetchCategories');
 
-            this.setupPagination(this.records.map(record => {
-                return {
-                    ...record,
-                    categoryName: categories.find(c => c.id === record.categoryID).title,
-                    typeClass: record.type === 'income' ? 'green' : 'red',
-                    typeText:  record.type === 'income' ? 'Доход' : 'Расход',
-
-                }
-            }));
+            this.setup(categories);
 
             this.loading = false;
         },
         methods: {
+            setup(categories) {
+                this.setupPagination(this.records.map(record => {
+                    return {
+                        ...record,
+                        categoryName: categories.find(c => c.id === record.categoryID).title,
+                        typeClass: record.type === 'income' ? 'green' : 'red',
+                        typeText:  record.type === 'income' ? 'Доход' : 'Расход',
 
-        }
+                    }
+                }));
+
+                this.renderChart(
+                    {
+                        //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        labels: categories.map(c => c.title),
+                        datasets: [{
+                            label: 'Расходы по категориям',
+                            data: categories.map(c => {
+                                return this.records.reduce((total,r) => {
+                                    if( r.categoryID === c.id && r.type === 'outcome' ) {
+                                        total += r.amount;
+                                    }
+                                    return total
+                                },0);
+                            }),
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.3)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.12)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)',
+                                'rgba(25, 159, 84, 0.25)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(25, 159, 84, .8)'
+                            ],
+                            borderWidth: .2
+                        }]
+                    },
+                    {} // options
+                );
+                //renderChart END
+            }
+        },
+
 
     }
 </script>
