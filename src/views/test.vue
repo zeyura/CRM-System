f4bfaf1de3dbccc158f6e3276035ccf1240f7bbd
zeyura <zeyura75@gmail.com>
2020-12-04 21:45:28 +0200

    add test file

---
+++ src/views/test.vue
@@ -0,0 +1,26 @@
+<template> </template>
+
+<script>
+import {mapGetters, mapActions} from '@'
+export default {
+    name: 'componentname',
+    components: {},
+    data: () => ({
+        arr = []
+    }),
+    mounted() {
+
+    },
+    methods: {
+        ...mapActions(['fn']),
+    },
+    computed: {
+        ...mapGetters(['info'])
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.class {
+}
+</style>
