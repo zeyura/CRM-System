084a0805c37b3895fe4cc3cf4b58273cab19a3ed
zeyura <zeyura75@gmail.com>
2020-12-04 21:57:38 +0200

    kill tester

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
-import Vue from 'vue'
-import Vuelidate from 'vuelidate'
-import VueMeta from 'vue-meta'
-import App from './App.vue'
-import router from './router/router'
-import store from './store/store'
-import dateFilter from './filters/date.filter'
-import currencyFilter from './filters/currency.filter'
-import localizeFilter from './filters/localize.filter'
-import messagePlugin from './utilites/message.plugin'
-import seoPlugin from './utilites/seo.plugin'
-import Loader from '@/components/app/Loader'
-import tooltipDirective from '@/directives/tooltip'
-import Paginate from 'vuejs-paginate'
+import Vue from "vue";
+import Vuelidate from "vuelidate";
+import VueMeta from "vue-meta";
+import App from "./App.vue";
+import router from "./router/router";
+import store from "./store/store";
+import dateFilter from "./filters/date.filter";
+import currencyFilter from "./filters/currency.filter";
+import localizeFilter from "./filters/localize.filter";
+import messagePlugin from "./utilites/message.plugin";
+import seoPlugin from "./utilites/seo.plugin";
+import Loader from "@/components/app/Loader";
+import tooltipDirective from "@/directives/tooltip";
+import Paginate from "vuejs-paginate";
 
-import VueSlider from 'vue-slider-component' // https://nightcatsama.github.io/vue-slider-component/
+import VueSlider from "vue-slider-component"; // https://nightcatsama.github.io/vue-slider-component/
 
-import './registerServiceWorker'
-import 'materialize-css/dist/js/materialize.min'
-import 'vue-slider-component/theme/antd.css'
+import "./registerServiceWorker";
+import "materialize-css/dist/js/materialize.min";
+import "vue-slider-component/theme/antd.css";
 
-///  FIREBASE  Import tester !!!
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
+///  FIREBASE  Import
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 ///
 
 Vue.config.productionTip = false;
@@ -32,27 +32,27 @@ Vue.use(Vuelidate);
 Vue.use(VueMeta);
 Vue.use(seoPlugin);
 
-Vue.filter('date', dateFilter); // register filter for Date -  dateFilter
-Vue.filter('currency', currencyFilter); // register filter for currencies -  currencyFilter
-Vue.filter('localize', localizeFilter);
+Vue.filter("date", dateFilter); // register filter for Date -  dateFilter
+Vue.filter("currency", currencyFilter); // register filter for currencies -  currencyFilter
+Vue.filter("localize", localizeFilter);
 
-Vue.directive('tooltip', tooltipDirective);
+Vue.directive("tooltip", tooltipDirective);
 
-Vue.component('Loader', Loader);
-Vue.component('Paginate', Paginate);
-Vue.component('VueSlider', VueSlider);
+Vue.component("Loader", Loader);
+Vue.component("Paginate", Paginate);
+Vue.component("VueSlider", VueSlider);
 
 /// --  https://www.youtube.com/watch?v=CTLonYohENw&list=PLqKQF2ojwm3njNpksFCi8o-_c-9Vva_W0&index=8  - - -
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA1cMEs91pSfNE3etXGW-9-ifp5AD-8jBc",
-    authDomain: "vue-crmsys.firebaseapp.com",
-    databaseURL: "https://vue-crmsys.firebaseio.com",
-    projectId: "vue-crmsys",
-    storageBucket: "vue-crmsys.appspot.com",
-    messagingSenderId: "760279348039",
-    appId: "1:760279348039:web:ae6f20269900caa27a1262",
-    measurementId: "G-FNDP5TV34G"
+  apiKey: "AIzaSyA1cMEs91pSfNE3etXGW-9-ifp5AD-8jBc",
+  authDomain: "vue-crmsys.firebaseapp.com",
+  databaseURL: "https://vue-crmsys.firebaseio.com",
+  projectId: "vue-crmsys",
+  storageBucket: "vue-crmsys.appspot.com",
+  messagingSenderId: "760279348039",
+  appId: "1:760279348039:web:ae6f20269900caa27a1262",
+  measurementId: "G-FNDP5TV34G"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -61,12 +61,12 @@ firebase.initializeApp(firebaseConfig);
 // инициализируем Vue если сработала аутентификация в  файрбейз - - -
 let app;
 firebase.auth().onAuthStateChanged(() => {
-    if(!app) { // проверка, чтоб приложение Vue  app  создавалось лиш  один раз )
-        app = new Vue({
-            router,
-            store,
-            render: h => h(App)
-        }).$mount('#app')
-    }
+  if (!app) {
+    // проверка, чтоб приложение Vue  app  создавалось лиш  один раз )
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
 });
-
